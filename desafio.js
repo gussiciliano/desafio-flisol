a04f66cb766b00bf034fe641453b1062bd99f648
Gustavo Hernan Siciliano <gussiciliano@gmail.com>
2025-04-26 00:02:00 -0300

    Update desafio.js

--- desafio.js
+++ desafio.js
@@ -2,14 +2,22 @@ function sonAnagramas(palabra1, palabra2) {
     const palabra1Lower = palabra1.toLowerCase();
     const palabra2Lower = palabra2.toLowerCase();
     const longitud = palabra1Lower.length;
+    const longitud2 = palabra2Lower.length;
+    
     let error = false;
 
     let i = 0;
+    let palabra2Aux = palabra2Lower;
+    let indice;
     while(i < longitud && !error) {
-        if (palabra1Lower[i] !== palabra2Lower[longitud - 1 - i]) {
-          error = true;
+        indice = palabra2Aux.indexOf(palabra1Lower[i]);
+        if (indice != -1) {
+          palabra2Aux = palabra2Aux.substring(0, indice) + palabra2Aux.substring(indice + 1);
+        }
+        else {
+            error = true;
         }
         i++
       }
-    return !error;
+    return !error && palabra2Aux.length == 0;
 }
